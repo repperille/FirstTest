6f96a54fa82ec09b3f3ef671c2ed26ccd6542c32
repperille <richard.e.persson@gmail.com>
2014-04-08 21:54:25 +0200

    Added file

---
+++ test.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use utf8;
+use Time::HiRes qw(usleep nanosleep);
+
+my $i = 0;
+while ($i < 1000000) {
+    print STDOUT $i, "\n";
+    $i++;
+    usleep(1000);
+}
